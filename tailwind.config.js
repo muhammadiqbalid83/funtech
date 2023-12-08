6ead80a569a70ecbc1aa5369312825b4e75cbc64
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-12-08 07:05:48 +0700

    style: color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      abu: '#3c3c3c',
+      hitam: '#000000',
+      'abu-hitam': '#131118',
+    },
     extend: {},
   },
   plugins: [],
