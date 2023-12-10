8e6f424e16f11fc3b31d224d86e476ac8ff05047
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-12-11 06:59:02 +0700

    feat: add a Collections component

---
+++ src/components/Collections.tsx
@@ -0,0 +1,12 @@
+import { Card } from '.'
+
+const Collections = () => {
+  return (
+    <div className="container mx-auto mt-[100px] w-full h-auto">
+      <h1 className="text-[32px] text-hitam text-center">New Collections</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default Collections
